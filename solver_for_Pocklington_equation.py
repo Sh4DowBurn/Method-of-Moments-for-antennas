8699c4b7fdbcc037c6989dd6a8d02fb284396d46
Sh4DowBurn <lukashovsergey28@gmail.com>
2025-01-07 06:02:48 +0300

    changed genetic alg

--- solver_for_Pocklington_equation.py
+++ solver_for_Pocklington_equation.py
@@ -33,6 +33,6 @@ def directional_pattern (frequency, delta_z, incident_voltage, element_position,
         Ei = lambda phi : np.sum(current_block[m]*np.exp(1j*wavenumber*R_block[m][:,1]*np.cos(phi))*np.exp(-1j*R_block[m][:,2]*np.sin(phi)))
         phi = np.linspace(1e-6, 2*np.pi-1e-6, 1000)
         E[m] = np.array([Ei(phi_i) for phi_i in phi])*(np.exp(delta_z*wavenumber*np.sin(phi))-1)/(wavenumber*np.sin(phi))
-    P_total = np.abs(np.sum(np.array(E), axis=0))
-    #P_total = P_total / np.max(P_total)
-    return R, R_block, element_num, incident_field, impedance, current, current_block, P_total, phi
+    E_total = np.abs(np.sum(np.array(E), axis=0))
+    #E_total = E_total / np.max(E_total)
+    return R, R_block, element_num, incident_field, impedance, current, current_block, E_total, phi
