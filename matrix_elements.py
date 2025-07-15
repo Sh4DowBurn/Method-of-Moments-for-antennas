99a6ed7d97967178ea75897ef75066b49bb75091
Sh4DowBurn <lukashovsergey28@gmail.com>
2025-07-15 23:48:36 +0300

    new impedance calc is working

--- matrix_elements.py
+++ matrix_elements.py
@@ -415,42 +415,100 @@ def Zmn_double (structure_type, basis_functions, m, n, i, j, segments_block, ome
     dr_m = delta_r * tau_m
     dr_n = delta_r * tau_n
     
-    if np.abs(np.dot(tau_m, tau_n)) <= 1e-21:
+    if np.abs(np.dot(tau_m, tau_n)) <= 1e-100:
         Z_0 = 0
     else:
         Z_0 = 1j*omega*mu0 / (4*np.pi) * np.dot(tau_m, tau_n) * delta_r**2 * (integrate.dblquad(ReGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args=(r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args=(r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
     
-    if np.abs(tau_m[0] * tau_n[0]) <= 1e-21:
+    if np.abs(tau_m[0] * tau_n[0]) <= 1e-100:
         Z_xx = 0
     else:
         Z_xx = 1j/(4*np.pi * omega * eps0) * tau_m[0] * tau_n[0] * delta_r**2  * (integrate.dblquad(RederderXXGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderXXGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
 
-    if np.abs(tau_m[1] * tau_n[1]) <= 1e-21:
+    if np.abs(tau_m[1] * tau_n[1]) <= 1e-100:
         Z_yy = 0
     else:
         Z_yy = 1j/(4*np.pi * omega * eps0) * tau_m[1] * tau_n[1] * delta_r**2  * (integrate.dblquad(RederderYYGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderYYGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
     
-    if np.abs(tau_m[2] * tau_n[2]) <= 1e-21:
+    if np.abs(tau_m[2] * tau_n[2]) <= 1e-100:
         Z_zz = 0
     else:
         Z_zz = 1j/(4*np.pi * omega * eps0) * tau_m[2] * tau_n[2] * delta_r**2  * (integrate.dblquad(RederderZZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderZZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
     
-    if np.abs(tau_n[0] * tau_m[1] + tau_n[1] * tau_m[0]) <= 1e-21:
+    if np.abs(tau_n[0] * tau_m[1] + tau_n[1] * tau_m[0]) <= 1e-100:
         Z_xy = 0
     else:
         Z_xy = 1j/(4*np.pi * omega * eps0) * (tau_n[0] * tau_m[1] + tau_n[1] * tau_m[0]) * delta_r**2  * (integrate.dblquad(RederderXYGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderXYGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
     
-    if np.abs(tau_n[0] * tau_m[2] + tau_n[2] * tau_m[0]) <= 1e-21:
+    if np.abs(tau_n[0] * tau_m[2] + tau_n[2] * tau_m[0]) <= 1e-100:
         Z_xz = 0
     else:
         Z_xz = 1j/(4*np.pi * omega * eps0) * (tau_n[0] * tau_m[2] + tau_n[2] * tau_m[0]) * delta_r**2  * (integrate.dblquad(RederderXZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderXZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
     
-    if np.abs(tau_n[2] * tau_m[1] + tau_n[1] * tau_m[2]) <= 1e-21:
+    if np.abs(tau_n[2] * tau_m[1] + tau_n[1] * tau_m[2]) <= 1e-100:
         Z_yz = 0
     else:
         Z_yz = 1j/(4*np.pi * omega * eps0) * (tau_n[2] * tau_m[1] + tau_n[1] * tau_m[2]) * delta_r**2  * (integrate.dblquad(RederderYZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderYZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
     return (Z_0 + Z_xx + Z_yy + Z_zz + Z_xy + Z_xz + Z_yz)
 
+def impedance_real(t_m, t_n, r_m, r_n, dr_m, dr_n, omega, basis_functions):
+    k = omega / c
+    
+    tau_n = dr_n / np.linalg.norm(dr_n)
+    tau_m = dr_m / np.linalg.norm(dr_m)
+    
+    c_0 = (np.dot(tau_m, tau_n))
+    c_x = (1/k**2 * tau_n[0] * tau_m[0])
+    c_y = (1/k**2 * tau_n[1] * tau_m[1])
+    c_z = (1/k**2 * tau_n[2] * tau_m[2])
+    c_xy = (1/k**2 * (tau_m[0]*tau_n[1] + tau_m[1]*tau_n[0]))
+    c_xz = (1/k**2 * (tau_m[0]*tau_n[2] + tau_m[2]*tau_n[0]))
+    c_yz = (1/k**2 * (tau_m[1]*tau_n[2] + tau_m[2]*tau_n[1]))
+
+    C = c_x + c_y + c_z
+
+    rmn =  (np.linalg.norm(r_m - r_n + dr_m*(t_m-1/2) - dr_n*(t_n-1/2)))
+    dx = (r_m[0] - r_n[0] + dr_m[0]*(t_m-1/2) - dr_n[0]*(t_n-1/2))
+    dy = (r_m[1] - r_n[1] + dr_m[1]*(t_m-1/2) - dr_n[1]*(t_n-1/2))
+    dz = (r_m[2] - r_n[2] + dr_m[2]*(t_m-1/2) - dr_n[2]*(t_n-1/2))
+    L = c_x * dx**2 + c_y * dy**2 + c_z * dz**2 + c_xy * dx * dy + c_xz * dx * dz + c_yz * dy * dz
+    
+    polypart1 = c_0 * rmn**2 - C + L * (3 - k**2 * rmn**2) / rmn**2
+    polypart2 = k * (3 * L / rmn - rmn * C)
+    
+    f_n = basis_func(basis_functions=basis_functions, t_n=t_n, r_n=r_n, dr_n=dr_n)
+    f_m = weight_func(basis_functions=basis_functions, t_m=t_m, r_m=r_m, dr_m=dr_m)
+    
+    return f_n * f_m * (polypart1 * np.cos(k * rmn) + polypart2 * np.sin(k * rmn)) / rmn**3
+def impedance_imag (t_m, t_n, r_m, r_n, dr_m, dr_n, omega, basis_functions):
+    k = omega / c
+    
+    tau_n = dr_n / np.linalg.norm(dr_n)
+    tau_m = dr_m / np.linalg.norm(dr_m)
+    
+    c_0 = (np.dot(tau_m, tau_n))
+    c_x = (1/k**2 * tau_n[0] * tau_m[0])
+    c_y = (1/k**2 * tau_n[1] * tau_m[1])
+    c_z = (1/k**2 * tau_n[2] * tau_m[2])
+    c_xy = (1/k**2 * (tau_m[0]*tau_n[1] + tau_m[1]*tau_n[0]))
+    c_xz = (1/k**2 * (tau_m[0]*tau_n[2] + tau_m[2]*tau_n[0]))
+    c_yz = (1/k**2 * (tau_m[1]*tau_n[2] + tau_m[2]*tau_n[1]))
+
+    C = c_x + c_y + c_z
+
+    rmn =  (np.linalg.norm(r_m - r_n + dr_m*(t_m-1/2) - dr_n*(t_n-1/2)))
+    dx = (r_m[0] - r_n[0] + dr_m[0]*(t_m-1/2) - dr_n[0]*(t_n-1/2))
+    dy = (r_m[1] - r_n[1] + dr_m[1]*(t_m-1/2) - dr_n[1]*(t_n-1/2))
+    dz = (r_m[2] - r_n[2] + dr_m[2]*(t_m-1/2) - dr_n[2]*(t_n-1/2))
+    L = c_x * dx**2 + c_y * dy**2 + c_z * dz**2 + c_xy * dx * dy + c_xz * dx * dz + c_yz * dy * dz
+    
+    polypart1 = c_0 * rmn**2 - C + L * (3 - k**2 * rmn**2) / rmn**2
+    polypart2 = k * (3 * L / rmn - rmn * C)
+    
+    f_n = basis_func(basis_functions=basis_functions, t_n=t_n, r_n=r_n, dr_n=dr_n)
+    f_m = weight_func(basis_functions=basis_functions, t_m=t_m, r_m=r_m, dr_m=dr_m)
+    
+    return f_n * f_m * (-polypart1 * np.sin(k * rmn) + polypart2 * np.cos(k * rmn)) / rmn**3
 
 def Zmn (structure_type, basis_functions, m, n, i, j, segments_block, omega, delta_r):
 
@@ -476,42 +534,7 @@ def Zmn (structure_type, basis_functions, m, n, i, j, segments_block, omega, del
     dr_m = delta_r * tau_m
     dr_n = delta_r * tau_n
     
-    if np.abs(np.dot(tau_m, tau_n)) <= 1e-21:
-        Z_0 = 0
-    else:
-        Z_0 = 1j*omega*mu0 / (4*np.pi) * np.dot(tau_m, tau_n) * delta_r**2 * (integrate.quad(ReGreen_function_single, t_min, t_max, args=(r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args=(r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    if np.abs(tau_m[0] * tau_n[0]) <= 1e-21:
-        Z_xx = 0
-    else:
-        Z_xx = 1j/(4*np.pi * omega * eps0) * tau_m[0] * tau_n[0] * delta_r**2  * (integrate.quad(RederderXXGreen_function_single, t_min, t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderXXGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    if np.abs(tau_m[1] * tau_n[1]) <= 1e-21:
-        Z_yy = 0
-    else:
-        Z_yy = 1j/(4*np.pi * omega * eps0) * tau_m[1] * tau_n[1] * delta_r**2  * (integrate.quad(RederderYYGreen_function_single, t_min, t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderYYGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    if np.abs(tau_m[2] * tau_n[2]) <= 1e-21:
-        Z_zz = 0
-    else:
-        Z_zz = 1j/(4*np.pi * omega * eps0) * tau_m[2] * tau_n[2] * delta_r**2  * (integrate.quad(RederderZZGreen_function_single, t_min, t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderZZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    if np.abs((tau_n[0] * tau_m[1] + tau_n[1] * tau_m[0])) <= 1e-21:
-        Z_xy = 0
-    else:
-        Z_xy = 1j/(4*np.pi * omega * eps0) * (tau_n[0] * tau_m[1] + tau_n[1] * tau_m[0]) * delta_r**2  * (integrate.quad(RederderXYGreen_function_single, t_min, t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderXYGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    if np.abs(tau_n[0] * tau_m[2] + tau_n[2] * tau_m[0]) <= 1e-21:
-        Z_xz = 0
-    else:
-        Z_xz = 1j/(4*np.pi * omega * eps0) * (tau_n[0] * tau_m[2] + tau_n[2] * tau_m[0]) * delta_r**2  * (integrate.quad(RederderXZGreen_function_single, t_min, t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderXZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    if np.abs(tau_n[2] * tau_m[1] + tau_n[1] * tau_m[2]) <= 1e-21:
-        Z_yz = 0
-    else:
-        Z_yz = 1j/(4*np.pi * omega * eps0) * (tau_n[2] * tau_m[1] + tau_n[1] * tau_m[2]) * delta_r**2  * (integrate.quad(RederderYZGreen_function_single, t_min, t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(ImderderYZGreen_function_double, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args = (r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
-    
-    return (Z_0 + Z_xx + Z_yy + Z_zz + Z_xy + Z_xz + Z_yz)
+    return 1j*omega*mu0 / (4*np.pi) * delta_r **2 * (integrate.dblquad(impedance_real, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args=(r_m, r_n, dr_m, dr_n, omega, basis_functions))[0] + 1j * integrate.dblquad(impedance_imag, t_min, t_max, lambda z1: t_min, lambda z2: t_max, args=(r_m, r_n, dr_m, dr_n, omega, basis_functions))[0])
 
 
 def calculate_impedance (basis_functions, structure_type, segments_block, frequency, delta_r):
@@ -532,7 +555,7 @@ def calculate_impedance (basis_functions, structure_type, segments_block, freque
                 impedance_mn = np.zeros((len(segments_block[m]), len(segments_block[n])), dtype=complex)
                 if m <= n :
                     for i in range (len(segments_block[m]) + len(segments_block[n])):
-                        impedance_mn[max(0, len(segments_block[m])-i-1), max(0, i-len(segments_block[m]))] = Zmn_double(structure_type=structure_type,basis_functions=basis_functions, m=m, n=n, i=max(0, len(segments_block[m])-i-1), j=max(0, i-len(segments_block[m])), segments_block=segments_block, omega=2*np.pi*frequency, delta_r=delta_r)
+                        impedance_mn[max(0, len(segments_block[m])-i-1), max(0, i-len(segments_block[m]))] = Zmn(structure_type=structure_type,basis_functions=basis_functions, m=m, n=n, i=max(0, len(segments_block[m])-i-1), j=max(0, i-len(segments_block[m])), segments_block=segments_block, omega=2*np.pi*frequency, delta_r=delta_r)
                         for k in range (min( min(len(segments_block[m]), len(segments_block[n])), i+1, len(segments_block[m]) + len(segments_block[n]) - i)):
                             impedance_mn[max(0, len(segments_block[m])-i-1) + k, max(0, i-len(segments_block[m])) + k] = impedance_mn[max(0, len(segments_block[m])-i-1), max(0, i-len(segments_block[m]))]
                 else :
@@ -550,7 +573,7 @@ def calculate_impedance (basis_functions, structure_type, segments_block, freque
                 if m <= n :
                     if m == n or np.linalg.norm(np.cross(tau_m, tau_n)) <= 1e-25:
                         for i in range (len(segments_block[m]) + len(segments_block[n])):
-                            impedance_mn[max(0, len(segments_block[m])-i-1), max(0, i-len(segments_block[m]))] = Zmn_double(structure_type=structure_type,basis_functions=basis_functions, m=m, n=n, i=max(0, len(segments_block[m])-i-1), j=max(0, i-len(segments_block[m])), segments_block=segments_block, omega=2*np.pi*frequency, delta_r=delta_r)
+                            impedance_mn[max(0, len(segments_block[m])-i-1), max(0, i-len(segments_block[m]))] = Zmn(structure_type=structure_type,basis_functions=basis_functions, m=m, n=n, i=max(0, len(segments_block[m])-i-1), j=max(0, i-len(segments_block[m])), segments_block=segments_block, omega=2*np.pi*frequency, delta_r=delta_r)
                             for k in range (min( min(len(segments_block[m]), len(segments_block[n])), i+1, len(segments_block[m]) + len(segments_block[n]) - i)):
                                 impedance_mn[max(0, len(segments_block[m])-i-1) + k, max(0, i-len(segments_block[m])) + k] = impedance_mn[max(0, len(segments_block[m])-i-1), max(0, i-len(segments_block[m]))]
                     else:
